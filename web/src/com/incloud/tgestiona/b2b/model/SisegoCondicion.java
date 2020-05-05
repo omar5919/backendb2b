1391f470db077840772c16ba3d8dd53bf44bf73e
emurgac <edwinmurgac@gmail.com>
2020-05-05 12:19:11 -0500

    se quita oferta detalle y se qda oferta detalle de oferta

---
+++ web/src/com/incloud/tgestiona/b2b/model/SisegoCondicion.java
@@ -0,0 +1,54 @@
+package com.incloud.tgestiona.b2b.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.incloud.tgestiona.domain.BaseDomain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "sisego_condicion",schema = "oferta")
+public class SisegoCondicion extends BaseDomain implements Identifiable<Integer>, Serializable {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sisego_condicion_id" ,nullable = true, precision = 10)
+	private Integer sisegoCondicionId ;
+	
+	@Column(name = "descripcion", length = 50 )
+	private String descripcion ;
+	@Column(name = "activo"  )
+	private Boolean activo ;
+	@Override
+	public String entityClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Integer getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setId(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean isIdSet() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
---
+++ web/src/com/incloud/tgestiona/b2b/model/ViaAcceso.java
@@ -0,0 +1,38 @@
+package com.incloud.tgestiona.b2b.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.incloud.tgestiona.b2b.model.oferta.Ofertas;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "via_acceso",schema = "oferta")
+public class ViaAcceso {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "via_acceso_id" ,nullable = true, precision = 10)
+	private Integer via_acceso_id;
+	
+	@Column(name = "codigo_isis", length = 2 )
+	private String codigo_isis ;
+	@Column(name = "descripcion", length = 60 )
+	private String descripcion ;
+	@Column(name = "tipo_servicio_id",  precision = 10 )
+	private Integer tipo_servicio_id ;
+	@Column(name = "activo" )
+	private Boolean activo ;
+}
